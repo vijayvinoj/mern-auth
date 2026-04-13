0286ac5a7d876f3fd0f50746302b1a292a743cfb
vijay <vijayvinoj88@gmail.com>
2026-04-13 20:09:15 +0530

    add express server entry point

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', require('./routes/authRoutes'));
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
