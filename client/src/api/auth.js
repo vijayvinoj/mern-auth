c4cd798e473ea15b442def3856f8968ffb314b59
vijay <vijayvinoj88@gmail.com>
2026-04-14 00:30:40 +0530

    add API layer:)

---
+++ client/src/api/auth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) req.headers.Authorization = `Bearer ${token}`;
+  return req;
+});
+
+export const registerUser = (data) => API.post('/auth/register', data);
+export const loginUser    = (data) => API.post('/auth/login', data);
+export const getMe        = ()     => API.get('/auth/me');
